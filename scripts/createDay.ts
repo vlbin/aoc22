2f9573477652ce60a1b1026522fd46ce9ffcdb2e
albin landgren <31692995+vlbin@users.noreply.github.com>
2022-11-29 21:47:19 +0100

    New file from template

--- scripts/createDay.ts
+++ scripts/createDay.ts
@@ -1,7 +1,7 @@
-import { mkdirSync, writeFileSync } from "fs";
+import { copyFileSync, mkdirSync, writeFileSync } from "fs";
 
 const day = process.argv[2];
 
 mkdirSync(`src/${day}`);
 writeFileSync(`src/${day}/input.txt`, "");
-writeFileSync(`src/${day}/solution.ts`, "");
+copyFileSync(`src/template/solution.ts`, `src/${day}/solution.ts`);
