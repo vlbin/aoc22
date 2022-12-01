3c2ca43d953251711c7fa059daef2f139b993092
albin landgren <31692995+vlbin@users.noreply.github.com>
2022-12-01 10:56:38 +0100

    Improved day 1 & template

--- src/01/solution.ts
+++ src/01/solution.ts
@@ -1,24 +1,25 @@
 import { readFile } from "fs/promises";
+import { timed } from "../utils/timed";
 
 const calories = (data: string) =>
-  data.split("\n\n").map((elv) =>
-    elv
-      .split("\n")
-      .map(Number)
-      .reduce((prev, curr) => prev + curr, 0)
-  );
+  data
+    .split("\n\n")
+    .map((elv) =>
+      elv.split("\n").reduce((prev, curr) => prev + Number(curr), 0)
+    );
 
 /* PART 1 */
-const one = (data: string) => Math.max(...calories(data));
+const one = (data: string) => calories(data).sort((a, b) => b - a)[0];
 
-readFile("input.txt", "utf-8").then((data) => console.log(one(data)));
+readFile("input.txt", "utf-8").then((data) => timed(1, () => one(data)));
 
 const threemost = (arr: number[]) =>
   arr
     .sort((a, b) => b - a)
-    .reduce((prev, curr, i) => (i > 2 ? prev : prev + curr), 0);
+    .slice(0, 3)
+    .reduce((prev, curr) => prev + curr, 0);
 
 /* PART 2 */
 const two = (data: string) => threemost(calories(data));
 
-readFile("input.txt", "utf-8").then((data) => console.log(two(data)));
+readFile("input.txt", "utf-8").then((data) => timed(2, () => two(data)));
