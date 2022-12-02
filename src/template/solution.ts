d1355adcede66d6a3a4def7fe4023b6cb6eefd12
albin landgren <31692995+vlbin@users.noreply.github.com>
2022-12-02 07:28:26 +0100

    day 2 cleanup & template fixes

--- src/template/solution.ts
+++ src/template/solution.ts
@@ -3,8 +3,10 @@ import { timed } from "../utils/timed";
 
 /* PART 1 */
 const one = (data: string) => {};
-readFile("input.txt", "utf-8").then((data) => timed(1, () => one(data)));
+readFile("input.txt", "utf-8").then((data) => console.log(one(data)));
+//readFile("input.txt", "utf-8").then((data) => timed(1, () => one(data)));
 
 /* PART 2 */
 const two = (data: string) => {};
-readFile("input.txt", "utf-8").then((data) => timed(2, () => two(data)));
+readFile("input.txt", "utf-8").then((data) => console.log(two(data)));
+//readFile("input.txt", "utf-8").then((data) => timed(2, () => two(data)));
