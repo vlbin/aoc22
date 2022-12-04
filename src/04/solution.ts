82eb1629c2e9f45be2eedaaec1d4a8f27657243f
albin landgren <31692995+vlbin@users.noreply.github.com>
2022-12-04 11:29:55 +0100

    refined day 4

--- src/04/solution.ts
+++ src/04/solution.ts
@@ -7,22 +7,18 @@ const overlap = (e1: number[], e2: number[]) =>
 const fullOverlap = (e1: number[], e2: number[]) =>
   (e1[0] <= e2[0] && e1[1] >= e2[1]) || (e2[0] <= e1[0] && e2[1] >= e1[1]);
 
-/* PART 1 */
-const one = (data: string) => {
-  return data
+const solve = (data: string, fn: (e1: number[], e2: number[]) => boolean) =>
+  data
     .split("\n")
     .map((l) => l.split(",").map((e) => e.split("-").map(Number)))
-    .reduce((prev, pair) => prev + (fullOverlap(pair[0], pair[1]) ? 1 : 0), 0);
-};
-readFile("input.txt", "utf-8").then((data) => console.log(one(data)));
-//readFile("input.txt", "utf-8").then((data) => timed(1, () => one(data)));
+    .reduce((prev, pair) => prev + (fn(pair[0], pair[1]) ? 1 : 0), 0);
+
+/* PART 1 */
+const one = (data: string) => solve(data, fullOverlap);
+//readFile("input.txt", "utf-8").then((data) => console.log(one(data)));
+readFile("input.txt", "utf-8").then((data) => timed(1, () => one(data)));
 
 /* PART 2 */
-const two = (data: string) => {
-  return data
-    .split("\n")
-    .map((l) => l.split(",").map((e) => e.split("-").map(Number)))
-    .reduce((prev, pair) => prev + (overlap(pair[0], pair[1]) ? 1 : 0), 0);
-};
-readFile("input.txt", "utf-8").then((data) => console.log(two(data)));
-//readFile("input.txt", "utf-8").then((data) => timed(2, () => two(data)));
+const two = (data: string) => solve(data, overlap);
+//readFile("input.txt", "utf-8").then((data) => console.log(two(data)));
+readFile("input.txt", "utf-8").then((data) => timed(2, () => two(data)));
