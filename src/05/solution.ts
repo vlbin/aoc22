b050c2a232912a080ffb1660f52dc93cffca43e8
albin landgren <31692995+vlbin@users.noreply.github.com>
2022-12-05 07:01:25 +0100

    slightly improved day 5

--- src/05/solution.ts
+++ src/05/solution.ts
@@ -1,26 +1,21 @@
 import { readFile } from "fs/promises";
 import { timed } from "../utils/timed";
 
+const transpose = <T>(matrix: T[][]) =>
+  matrix[0].map((_, ci) => matrix.map((r) => r[ci]));
+
 const parseCrates = (input: string) => {
   const lines = input.split("\n\n")[0].split("\n");
   const data = lines
     .map((line) => {
       return line
         .split("")
-        .map((c, i, cs) => {
-          if (i % 4 === 1) {
-            return c.length ? c : "empty";
-          }
-          return "";
-        })
-        .filter((x) => x != "");
+        .map((c, i) => (i % 4 === 1 ? c : ""))
+        .filter((x) => x.length);
     })
-    .filter((x) => !x.includes("1"));
-  const crates = data[0]
-    .map((_, ci) => data.map((r) => r[ci]))
-    .map((x) => x.filter((x) => x != " "));
+    .slice(0, -1);
 
-  return crates;
+  return transpose(data).map((x) => x.filter((x) => x != " "));
 };
 
 const parseInstr = (input: string) => {
@@ -30,18 +25,14 @@ const parseInstr = (input: string) => {
     .map((instr) =>
       instr
         .split(" ")
-        .filter((x, i) => i % 2 === 1)
+        .filter((_, i) => i % 2 === 1)
         .map(Number)
     )
-    .map((l) => ({
-      n: l[0],
-      from: l[1],
-      to: l[2],
+    .map(([n, from, to]) => ({
+      n,
+      from,
+      to,
     }));
-};
-
-const take = <T>(list: T[], n: number) => {
-  return list.splice(0, n);
 };
 
 /* PART 1 */
@@ -58,20 +49,20 @@ const one = (data: string) => {
   });
   return crates.map((x) => x[0]).join("");
 };
-readFile("input.txt", "utf-8").then((data) => console.log(one(data)));
-//readFile("input.txt", "utf-8").then((data) => timed(1, () => one(data)));
+
+//readFile("input.txt", "utf-8").then((data) => console.log(one(data)));
+readFile("input.txt", "utf-8").then((data) => timed(1, () => one(data)));
 
 /* PART 2 */
 const two = (data: string) => {
   const crates = parseCrates(data);
   const instrs = parseInstr(data);
 
-  instrs.forEach((instr, i) => {
-    console.log(crates);
-
-    crates[instr.to - 1].unshift(...take(crates[instr.from - 1], instr.n));
+  instrs.forEach((instr) => {
+    crates[instr.to - 1].unshift(...crates[instr.from - 1].splice(0, instr.n));
   });
   return crates.map((x) => x[0]).join("");
 };
-readFile("input.txt", "utf-8").then((data) => console.log(two(data)));
-//readFile("input.txt", "utf-8").then((data) => timed(2, () => two(data)));
+
+//readFile("input.txt", "utf-8").then((data) => console.log(two(data)));
+readFile("input.txt", "utf-8").then((data) => timed(2, () => two(data)));
