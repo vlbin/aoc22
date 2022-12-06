14ea9a5b2382b8dc543cdd455ac87c69430018e6
albin landgren <31692995+vlbin@users.noreply.github.com>
2022-12-06 07:19:04 +0100

    cleaned up day 6

--- src/06/solution.ts
+++ src/06/solution.ts
@@ -1,24 +1,17 @@
 import { readFile } from "fs/promises";
 import { timed } from "../utils/timed";
 
+const readPacket = (arr: string[], nChars: number) =>
+  arr.findIndex(
+    (c, i, arr) => new Set(arr.slice(i, i + nChars)).size === nChars
+  ) + nChars;
+
 /* PART 1 */
-const one = (data: string) => {
-  return (
-    data.split("").findIndex((c, i, arr) => {
-      return new Set([arr[i], arr[i + 1], arr[i + 2], arr[i + 3]]).size === 4;
-    }) + 4
-  );
-};
-readFile("input.txt", "utf-8").then((data) => console.log(one(data)));
-//readFile("input.txt", "utf-8").then((data) => timed(1, () => one(data)));
+const one = (data: string) => readPacket(data.split(""), 4);
+//readFile("input.txt", "utf-8").then((data) => console.log(one(data)));
+readFile("input.txt", "utf-8").then((data) => timed(1, () => one(data)));
 
 /* PART 2 */
-const two = (data: string) => {
-  return (
-    data.split("").findIndex((c, i, arr) => {
-      return new Set(arr.slice(i, i + 14)).size === 14;
-    }) + 14
-  );
-};
-readFile("input.txt", "utf-8").then((data) => console.log(two(data)));
-//readFile("input.txt", "utf-8").then((data) => timed(2, () => two(data)));
+const two = (data: string) => readPacket(data.split(""), 14);
+//readFile("input.txt", "utf-8").then((data) => console.log(two(data)));
+readFile("input.txt", "utf-8").then((data) => timed(2, () => two(data)));
